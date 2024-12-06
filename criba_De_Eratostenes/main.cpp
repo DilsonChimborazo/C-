6b870edbbce5a748464947755908b9891c93cf91
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-12-06 00:43:39 -0500

    tercer ejercicio

---
+++ criba_De_Eratostenes/main.cpp
@@ -0,0 +1,10 @@
+#include "cribaEratostenes.hpp"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int valor;
+    cout << "Ingrese un numero: ";
+    cin >> valor;
+    cribaEratostenes(valor);
+}
