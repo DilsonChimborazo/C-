a09d1c23fac1ac7b4326f3fdb2ae8dec33af27dd
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-12-06 01:16:05 -0500

    Sexto ejercicio

---
+++ frecuencia_De_Caracteres/frecuencia.cpp
@@ -0,0 +1,24 @@
+#include "frecuencia.hpp"
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+char caracterMasRepetido(const string& texto) {
+    unordered_map<char, int> frecuencia;
+    for (char c : texto) {
+        frecuencia[c]++;
+    }
+
+    char maxCaracter = texto[0];
+    int maxFrecuencia = frecuencia[maxCaracter];
+
+    for (const auto& par : frecuencia) {
+        if (par.second > maxFrecuencia) {
+            maxCaracter = par.first;
+            maxFrecuencia = par.second;
+        }
+    }
+
+    return maxCaracter;
+}
---
+++ frecuencia_De_Caracteres/frecuencia.hpp
@@ -0,0 +1,10 @@
+#ifndef FRECUENCIA_HPP
+#define FRECUENCIA_HPP
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+char caracterMasRepetido(const string& texto);
+
+#endif
