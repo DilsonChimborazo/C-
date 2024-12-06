f540797ad6508b28caec370d3416034338bd7eef
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-12-06 01:25:11 -0500

    Septimo ejercicio

---
+++ sudoku_Checker/sudoku.hpp
@@ -0,0 +1,10 @@
+#ifndef SUDOKU_HPP
+#define SUDOKU_HPP
+
+#include <array>
+using namespace std;
+
+// Declaración de la función que verifica si un tablero de Sudoku es válido
+bool esTableroValido(const array<array<int, 9>, 9>& tablero);
+
+#endif
