f1f599ed2821da3a2c3e35b1ec6ffbe1f3e20ae4
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-12-06 00:55:14 -0500

    Cuarto ejercicio

---
+++ triangulo_Pascal/main.cpp
@@ -0,0 +1,11 @@
+#include "trianguloPascal.hpp"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int alt;
+    cout << "Ingrese la altura del triangulo: ";
+    cin >> alt;
+    trianguloPascal(alt);
+    return 0;
+}
